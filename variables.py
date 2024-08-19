3a312a467f0d30532b79b92f58ad02825cf21030
brian211002 <brian211002@gmail.com>
2024-08-19 15:55:13 -0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,18 @@
+#Definicion de variables
+X=2
+Y=5
+#Ejecucion
+Resultado = X * Y
+#resultado en pantalla
+print("resulado de la multiplicacion", Resultado)
+
+variable1= "Brian"
+variable2= "Gomez"
+
+concantenacion= " mi nombre completo es " + variable1 + " " + variable2
+print(concantenacion)
+
+nombre= input("ingrese su nombre:")
+dni= input("ingrese su DNI:")
+
+print(f"el nombre es {nombre} y su dni es {dni}")
